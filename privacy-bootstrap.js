70e33016155cf8b66af27e392a1267378a6d391a
omargallardoc1-bit <omar.gallardoc1@gmail.com>
2026-08-20 15:54:45 -0700

    Retirar bootstrap no utilizado

--- privacy-bootstrap.js
+++
@@ -1,8 +0,0 @@
-// Carga la integración de privacidad sin modificar el bundle principal.
-// Vercel sirve este archivo como recurso estático.
-(function(){
-  var script=document.createElement('script');
-  script.src='/privacy-integration.js';
-  script.defer=true;
-  document.head.appendChild(script);
-})();
